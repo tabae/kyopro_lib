b532034adac93a87dedba843d87dc83aaadfa9b8
Ebata <dongurinao@gmail.com>
2020-10-08 01:12:00 +0900

    osa_k is updated

--- math/osa_k/abc177_e.cpp
+++ math/osa_k/abc177_e.cpp
@@ -14,36 +14,43 @@ ll d1[4] = {1, -1, 0, 0};
 ll d2[4] = {0, 0, 1, -1};
 
 /* --------- ここから --------- */
-using _ll = long long;
+struct osa_k {
+    using _ll = long long;
 
-#define PRIME_FACTORIZE_MAX 2000010
-_ll primes[PRIME_FACTORIZE_MAX];
+    #define PRIME_FACTORIZE_MAX 2000010
+    vector<_ll> primes;
 
-void pre_osa_k() {
-    for(_ll i = 0; i < PRIME_FACTORIZE_MAX; i++) {
-        primes[i] = 1;
-    }
-    for(_ll i = 2; i < PRIME_FACTORIZE_MAX; i++) {
-        if(primes[i] != 1) continue;
-        for(_ll j = i; j <= PRIME_FACTORIZE_MAX; j += i) {
-            primes[j] = i;
+    osa_k() {
+        primes.resize((PRIME_FACTORIZE_MAX));
+        for(_ll i = 0; i < PRIME_FACTORIZE_MAX; i++) {
+            primes[i] = 1;
+        }
+        for(_ll i = 2; i < PRIME_FACTORIZE_MAX; i++) {
+            if(primes[i] != 1) continue;
+            for(_ll j = i; j <= PRIME_FACTORIZE_MAX; j += i) {
+                primes[j] = i;
+            }
         }
     }
-}
 
-vector<pair<_ll,_ll>> factorize(_ll n){
-    vector<pair<_ll,_ll>> res;
-    while(n != 1) {
-        _ll cur = primes[n];
-        _ll cnt = 0;
-        while(n % cur == 0) {
-            n /= cur;
-            cnt++;
+    vector<pair<_ll,_ll>> factorize(_ll n){
+        vector<pair<_ll,_ll>> res;
+        while(n != 1) {
+            _ll cur = primes[n];
+            _ll cnt = 0;
+            while(n % cur == 0) {
+                n /= cur;
+                cnt++;
+            }
+            res.push_back({cur, cnt});
         }
-        res.push_back({cur, cnt});
+        reverse(res.begin(), res.end());
+        return res;
     }
-    reverse(res.begin(), res.end());
-    return res;
+} osa_k;
+
+vector<pair<long long, long long>>factorize(long long n) {
+    return osa_k.factorize(n);
 }
 /* --------- ここまで --------- */
 
@@ -70,7 +77,6 @@ int main(){
     rep(i, 0, n) cin >> a[i];
     bool pair_wise = true;
     set<ll> seen;
-    pre_osa_k();
     rep(i, 0, n) {
         auto v = factorize(a[i]);
         for(auto e : v) {
--- math/osa_k/sample.cpp
+++ math/osa_k/sample.cpp
@@ -2,43 +2,47 @@
 using namespace std;
 
 /* --------- ここから --------- */
-using _ll = long long;
+struct osa_k {
+    using _ll = long long;
 
-#define PRIME_FACTORIZE_MAX 2000010
-_ll primes[PRIME_FACTORIZE_MAX];
+    #define PRIME_FACTORIZE_MAX 2000010
+    vector<_ll> primes;
 
-void pre_osa_k() {
-    for(_ll i = 0; i < PRIME_FACTORIZE_MAX; i++) {
-        primes[i] = 1;
-    }
-    for(_ll i = 2; i < PRIME_FACTORIZE_MAX; i++) {
-        if(primes[i] != 1) continue;
-        for(_ll j = i; j <= PRIME_FACTORIZE_MAX; j += i) {
-            primes[j] = i;
+    osa_k() {
+        primes.resize((PRIME_FACTORIZE_MAX));
+        for(_ll i = 0; i < PRIME_FACTORIZE_MAX; i++) {
+            primes[i] = 1;
+        }
+        for(_ll i = 2; i < PRIME_FACTORIZE_MAX; i++) {
+            if(primes[i] != 1) continue;
+            for(_ll j = i; j <= PRIME_FACTORIZE_MAX; j += i) {
+                primes[j] = i;
+            }
         }
     }
-}
 
-vector<pair<_ll,_ll>> factorize(_ll n){
-    vector<pair<_ll,_ll>> res;
-    while(n != 1) {
-        _ll cur = primes[n];
-        _ll cnt = 0;
-        while(n % cur == 0) {
-            n /= cur;
-            cnt++;
+    vector<pair<_ll,_ll>> factorize(_ll n){
+        vector<pair<_ll,_ll>> res;
+        while(n != 1) {
+            _ll cur = primes[n];
+            _ll cnt = 0;
+            while(n % cur == 0) {
+                n /= cur;
+                cnt++;
+            }
+            res.push_back({cur, cnt});
         }
-        res.push_back({cur, cnt});
+        reverse(res.begin(), res.end());
+        return res;
     }
-    reverse(res.begin(), res.end());
-    return res;
+} osa_k;
+
+vector<pair<long long, long long>>factorize(long long n) {
+    return osa_k.factorize(n);
 }
 /* --------- ここまで --------- */
 
 int main() {
-
-    // 前処理
-    pre_osa_k();
 
     int n;
     cin >> n;
