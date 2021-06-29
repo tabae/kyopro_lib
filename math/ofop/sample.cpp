d745000586fb6d0c7475832e7ad9a5a6f9972c45
Ebata <dongurinao@gmail.com>
2021-06-29 22:04:20 +0900

    [math/ofop] Bugfix: Remove zero division

--- math/ofop/sample.cpp
+++ math/ofop/sample.cpp
@@ -5,6 +5,7 @@ constexpr long long nmax = 1000000000000000000; // 10^18
 // nmaxより大きくなるとき, nmax+1を返す
 long long ofpow(long long a, long long p) {
     if(a > nmax) return nmax + 1;
+    if(a == 0) return 0;
     long long res = 1;
     for(int i = 0; i < p; i++) {
         if(nmax / a < res) return nmax + 1;
@@ -16,6 +17,7 @@ long long ofpow(long long a, long long p) {
 // nmaxより大きくなるとき, nmax+1を返す
 long long ofmul(long long a, long long b) {
     if(a > nmax || b > nmax) return nmax + 1;
+    if(a == 0 || b == 0) return 0;
     if(nmax / a < b) return nmax + 1;
     return a * b;
 }
